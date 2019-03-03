cbcfc637e738db93f72eeed2a0682b38d3c5a4e0
Piotr Dąbrowski <piotrek816@wp.pl>
2019-03-03 17:30:59 +0100

    w

--- liveDemo/Form1.Designer.cs
+++ liveDemo/Form1.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(719, 73);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(719, 550);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(719, 623);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- liveDemo/Form1.cs
+++ liveDemo/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static MST.MES.OrderStructureByOrderNo;
 
 namespace liveDemo
 {
@@ -16,5 +17,49 @@ namespace liveDemo
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Add("a", "Zlecenie");
+            dataGridView1.Columns.Add("s", "Kitting");
+            dataGridView1.Columns.Add("d", "10NC");
+            dataGridView1.Columns.Add("z", "Ilosc");
+            dataGridView1.Columns.Add("x", "SMT");
+            dataGridView1.Columns.Add("c", "Test");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var kittingData = MST.MES.SqlDataReaderMethods.Kitting.GetOrdersInfoByDataReader(7);
+            var notFinishedOrder = kittingData.Where(o => o.Value.endDate.Year < 2017);
+            var lgOrders = notFinishedOrder.Select(o=>o.Value).Where(o => o.productionPlanId != "0").GroupBy(o=>o.productionPlanId).ToDictionary(k=>k.Key, v=>v.ToList());
+            var mstOrders = notFinishedOrder.Where(o => o.Value.productionPlanId == "0");
+
+            var smtData = MST.MES.SqlDataReaderMethods.SMT.GetOrders(notFinishedOrder.Select(o => o.Key).ToArray());
+            var testData = MST.MES.SqlDataReaderMethods.LedTest.GetTestRecordsForOrders(MST.MES.SqlDataReaderMethods.LedTest.TesterIdToName(),mstOrders.Select(o => o.Key).ToArray());
+
+            var lgPlanFullInfo = MST.MES.SqlDataReaderMethods.Kitting.GetKittingDataForProductionPlan(lgOrders.Select(o => o.Key).ToArray());
+
+            foreach (var order in mstOrders)
+            {
+                dataGridView1.Rows.Add(order.Value.orderNo, 
+                                        order.Value.kittingDate, 
+                                        order.Value.modelId_12NCFormat, 
+                                        order.Value.orderedQty,
+                                        smtData[order.Key].totalManufacturedQty,
+                                        testData[order.Key].totalPcbsTestedCount);
+            }
+
+            foreach (var prodPlanEntry in lgOrders)
+            {
+                dataGridView1.Rows.Add(prodPlanEntry.Key, 
+                    prodPlanEntry.Value.First().kittingDate, 
+                    prodPlanEntry.Value.First().modelId_12NCFormat, 
+                    lgPlanFullInfo[prodPlanEntry.Key].Select(o => o.orderedQty).Sum()
+                    );
+            }
+        }
+
+        
     }
 }
